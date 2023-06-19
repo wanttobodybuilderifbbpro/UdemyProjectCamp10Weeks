4154990f80343df5143c2d66fc45ab47059c34d1
박재형 <wogud114@gmail.com>
2023-06-19 22:13:59 +0900

    오늘실습복습

---
+++ 230619/실습03.js
@@ -0,0 +1,35 @@
+const taskForm = document.getElementById("task-form");
+const taskInput = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
+
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const task = taskInput.ariaValueMax;
+  if(task){
+    addTask(task);
+    taskInput.value = "";
+  }
+});
+
+taskList.addEventListener("click", (e)=>{
+  if(e.target.tagName === "BUTTON"){
+    deleteTask(e.target.parentElement);
+  }
+});
+
+function addTask(task){
+  const taskItem = document.createElement("li");
+  taskItem.classList.add("task-item");
+  taskItem.textContent = task;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "삭제";
+
+  taskItem.appendChild(deleteButton);
+  taskList.appendChild(taskItem);
+}
+
+function deleteTask(tasKItem) {
+  taskList.removeChild(taskItem);
+}
