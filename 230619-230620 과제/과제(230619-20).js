4fb7e7f2638f2120212933d42ee6ffcef6860828
박재형 <wogud114@gmail.com>
2023-06-20 12:32:14 +0900

    temporarily finished

---
+++ 230619-230620 과제/과제(230619-20).js
@@ -0,0 +1,27 @@
+function startTime(){
+  let today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML = h +":" + m + ":" + s;
+  let t = setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if(i < 10) {i= "0" + i};
+  return i;
+}
+
+let images =[
+  "사진들/건물.jpg",
+  "사진들/바다.jpg",
+  "사진들/천체.jpg",
+];
+
+function clickImg(){
+  let image = document.getElementById("image");
+  let random = Math.floor(Math.random() * images.length);
+  image.src = images[random];
+}
