bcc9c126c95917f4032ba70225a7a3c3a990450c
박재형 <wogud114@gmail.com>
2023-07-15 23:34:20 +0900

    I have to finish the works.

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,5 +1,39 @@
-import React from "react";
+import { dbService } from "fBase";
+import { addDoc, collection  } from "firebase/firestore";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>
+const Home = () => {
+  const [nweet, setNweet] = useState("");
+   const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"),{
+        nweet,
+        createdAt:Date.now(),
+      });
+      console.log("Document written with ID:", docRef.id);
+    } catch (error) {
+      console.log("Error adding document:", error);
+    }
+    setNweet("");
+   };
+   const onChange = ({target:{value}}) => {
+    setNweet(value);
+   };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input 
+          value={nweet} 
+          onChange={onChange} 
+          type="text" 
+          placeholder="What's on your mind?" 
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+ );
+};
 
 export default Home;
